d748d2bce2a4407fa9051af189d303ec28e03fc3
codeandcofee <yvrkarthik@gmail.com>
2018-09-02 20:51:46 +1200

    Added Initial migration

---
+++ Sqlite.Infrastructure/Migrations/20180902085101_InitialMigration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sqlite.Infrastructure.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExpenseTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LastModifiedOn = table.Column<DateTime>(nullable: true),
+                    AddedOn = table.Column<DateTime>(nullable: true),
+                    Type = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseTypes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExpenseTypes");
+        }
+    }
+}
