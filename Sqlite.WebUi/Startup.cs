0b8c2d24d41f6310ad08d288c7b435cebb008cf2
codeandcofee <yvrkarthik@gmail.com>
2018-09-02 20:50:35 +1200

    Add AppDbContext to DI in the WebUI

--- Sqlite.WebUi/Startup.cs
+++ Sqlite.WebUi/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Sqlite.Infrastructure.Data;
 
 namespace Sqlite.WebUi
 {
@@ -20,7 +22,7 @@ namespace Sqlite.WebUi
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            //services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=expensetracking.db"));
+            services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=expensetracking.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
