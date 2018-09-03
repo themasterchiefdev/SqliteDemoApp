d984dac291c3ec1334d65f7110202d9574d8248a
codeandcofee <yvrkarthik@gmail.com>
2018-09-04 11:13:39 +1200

    Introduced Service layer

---
+++ Sqlite.Infrastructure/Services/ExpenseTypeService/ExpenseTypeService.cs
@@ -0,0 +1,52 @@
+﻿using Sqlite.Core.Entities;
+using Sqlite.Core.Interfaces.Repositories;
+using Sqlite.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sqlite.Infrastructure.Services.ExpenseTypeService
+{
+    /// <summary>
+    /// This class acts as a glue between ExpenseTypeRepository and the webui.
+    /// </summary>
+    public class ExpenseTypeService : IExpenseTypeService
+    {
+        private readonly IExpenseTypeRepository _repository;
+
+        public ExpenseTypeService(IExpenseTypeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void CreateExpenseType(ExpenseType expenseType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditExpenseType(ExpenseType expenseType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveExpenseType(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ExpenseType> GetAllExpenses()
+        {
+            return _repository.GetAllExpenses().ToList();
+        }
+
+        public ExpenseType GetExpenseTypeByName(string expenseTypeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateExpenseType(ExpenseType expenseTypeToValidate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
